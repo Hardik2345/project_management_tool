0086ae96accdd750f29c18855a74518a79cc1510
Hardik Parikh <hardikparikh19@gmail.com>
2025-07-19 17:17:27 +0530

    testing new login

--- Tool/project_management_tool/controllers/authController.js
+++ Tool/project_management_tool/controllers/authController.js
@@ -92,7 +92,24 @@ exports.logout = (req, res) => {
   });
 };
 
-exports.protect = passport.authenticate("jwt", { session: false });
+exports.protect = catchAsync(async (req, res, next) => {
+  //1) Get the token and check if it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please login to get access.", 401)
+    );
+  }
+});
 
 exports.isLoggedIn = async (req, res, next) => {
   if (req.cookies.jwt && req.cookies.jwt !== "loggedout") {
