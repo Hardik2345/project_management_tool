b12f4459a646f57847cb31cfe277098a1b5adcaf
Hardik Parikh <hardikparikh19@gmail.com>
2025-07-19 14:06:24 +0530

    fixed same site error

--- Tool/project_management_tool/controllers/authController.js
+++ Tool/project_management_tool/controllers/authController.js
@@ -20,8 +20,8 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
-    sameSite: "Lax",
+    secure: true, // must be secure for cross-site
+    sameSite: "None", // allow cross-site cookies
   });
   user.password = undefined;
   res.status(statusCode).json({
