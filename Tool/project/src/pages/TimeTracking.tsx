8623cee1423906edc8c6f50683ab3d94a5cd9c8a
Hardik Parikh <hardikparikh19@gmail.com>
2025-07-20 13:34:19 +0530

    added some more logging

--- Tool/project/src/pages/TimeTracking.tsx
+++ Tool/project/src/pages/TimeTracking.tsx
@@ -450,6 +450,7 @@ export function TimeTracking() {
   };
 
   const filteredTimeEntries = getFilteredTimeEntries();
+  console.log("real filteredTimeEntries", filteredTimeEntries);
   const totalHours =
     filteredTimeEntries.reduce((sum, te) => sum + te.duration, 0) / 60;
 
@@ -633,7 +634,7 @@ export function TimeTracking() {
                     >
                       <option value="">Select project...</option>
                       {state.projects.map((project) => (
-                        <option key={project.id} value={project.id}>
+                        <option key={project._id} value={project._id}>
                           {project.name}
                         </option>
                       ))}
