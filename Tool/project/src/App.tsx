2e5cb289d3a76381d887634875def9f50552193b
Hardik Parikh <hardik@Hardiks-MacBook-Air.local>
2025-07-27 12:45:36 +0530

    fixing login flow

--- Tool/project/src/App.tsx
+++ Tool/project/src/App.tsx
@@ -43,7 +43,6 @@ function AppContent() {
       {!currentUser ? (
         <>
           <Route path="/login" element={<Login />} />
-          <Route path="*" element={<Navigate to="/login" />} />
         </>
       ) : (
         <>
