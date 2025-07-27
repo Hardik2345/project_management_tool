5dde8dcd4c3b463c418c845342eb5e7b4306b846
Hardik Parikh <hardik@Hardiks-MacBook-Air.local>
2025-07-27 18:41:12 +0530

    excluding tasks that has status of done

--- Tool/project_management_tool/utils/mail.js
+++ Tool/project_management_tool/utils/mail.js
@@ -79,7 +79,7 @@ const taskDueTomorrow = async (assignedTo) => {
 
 function startEmailScheduler() {
   cron.schedule(
-    '36 18 * * *',
+    '30 19 * * *',
     async () => {
       console.log('Running daily email task');
       const users = await members();
